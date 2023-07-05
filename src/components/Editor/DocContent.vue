cfce9bbce4351f9067a7311e742944134f223099
fangxiaoxun <1272449367@qq.com>
2023-07-05 18:11:20 +0800

    fix: 修改加粗文本bug

--- src/components/Editor/DocContent.vue
+++ src/components/Editor/DocContent.vue
@@ -1,45 +1,17 @@
 <script lang="ts" setup>
-import { ref, onMounted, nextTick } from 'vue'
+import { ref } from 'vue'
 const editor = ref<HTMLElement>(document.createElement('div'))
-const docContent = ref<string>('hhhhh')
-// 判断是否进行中文输入
-const composing = ref<boolean>(false);
-const title = ref<string>('')
-const handleCompositionStart = () => {
-    composing.value = true;
-    console.log('开始输入中文')
-}
 
-// 中文输入的时候触发函数
-const handleCompositionUpdate = (event: Event): void => {
-    if (composing.value) {
-        docContent.value = (event.target as HTMLElement).innerText
-    }
-    console.log((event.target as HTMLElement).innerText)
-    console.log('update触发')
-}
-
-const handleCompositionEnd = (event: CompositionEvent): void => {
-    console.log("结束输入")
-    if (composing.value) {
-        composing.value = false
-        docContent.value = (event.target as HTMLElement).innerText
-    }
-}
-
-onMounted(async () => {
-    await nextTick();
-    editor.value.focus();
-})
-
-
-// 解决Dom结果不一致
+// onMounted(async () => {
+//     await nextTick();
+//     editor.value.focus();
+// })
 
 interface TNode {
     type: string;
-    attrs: Record<string, any>;
-    content?: string;
-    marks?: TNode[];
+    attrs: Record<string, any>;     //节点标签样式
+    content?: string;   //节点存储内容
+    marks?: TNode[];    //标签类型
     children?: TNode[];
 }
 // 文段
@@ -48,14 +20,14 @@ const paragraph: TNode = {
     attrs: {}
 };
 // 文字
-const textNode: TNode = {
+const text: TNode = {
     type: 'text',
     attrs: {}
 };
 
 // 加粗
 const strong: TNode = {
-    type: 'strong',
+    type: 'b',
     attrs: {}
 };
 
@@ -68,9 +40,9 @@ const em: TNode = {
 const state: TNode[] = [{
     ...paragraph,
     children: [
-        { ...textNode, content: 'text ' },
-        { ...textNode, marks: [strong], content: 'strong' },
-        { ...textNode, marks: [strong, em], content: 'italic text' },
+        { ...text, content: 'text ' },
+        { ...text, marks: [strong], content: 'strong' },
+        { ...text, marks: [strong, em], content: 'italic text' },
     ]
 }];
 console.log(state)
@@ -111,86 +83,55 @@ const handleInput = (event: Event): any => {
 function parseInput(input: string): TNode {
     const rootNode: TNode = { type: 'root', attrs: {}, children: [] };
     // 根据输入内容解析并构建树形结构
-    const lines = input.split('\n');
-    console.log(lines)
-    for (const line of lines) {
-        const trimmedLine = line.trim();
-        if (trimmedLine.startsWith('<p')) {
-            // 获取p标签中的内容
-            let content = trimmedLine.substring(trimmedLine.indexOf('>') + 1, trimmedLine.length - 4);
-            const paragraphNode: TNode = { type: 'p', attrs: {}, content: '', children: [] };
+    console.log(input)
+    const regex = /<(?:p|div)[^>]*>(.*?)<\/(?:p|div)>/g;
+    const matches = input.matchAll(regex);
 
+    const paragraphs = Array.from(matches, match => match[1]);
+    console.log(paragraphs)
+    for (const line of paragraphs) {
+            // 获取p标签中的内容
+            let content = line
+            // p节点
+            const paragraphNode: TNode = { ...paragraph, children: [] };
+            // 存在 b 标签
             if (content.includes('<b>')) {
                 // 匹配加粗的文字,数组形式
                 const boldMatches = content.match(/<b>(.*?)<\/b>/g);
-                // boldMatches?.forEach((substr => {
-                //     // 匹配文字段
-                //     console.log(substr)
-                // }))
-                // console.log(boldMatches)
                 // 加粗处理
                 if (boldMatches) {
                     // 开始进行text子串push
                     for (let i = 0; i < boldMatches.length; i++) {
                         let str = content.substring(0, content.indexOf(boldMatches[i]))
-
                         content = content.replace(boldMatches[i], '')//删除b标签
                         //  push text标签
                         if (str) {
-                            const textNode: TNode = { type: 'text', attrs: {}, content: str };
+                            const textNode: TNode = { ...text, content: str };
                             paragraphNode.children?.push(textNode)
                         }
                         content = content.replace(str, '')
-
                         const Btext = boldMatches[i].substring(boldMatches[i].indexOf('<b>') + 3, boldMatches[i].indexOf('</b>'))
-                        const boldText: TNode = { type: 'b', attrs: {}, children: [], content: Btext }
+                        const boldText: TNode = { ...strong, children: [], content: Btext }
                         // push进children数组
-                        console.log(Btext)
                         paragraphNode.children?.push(boldText)
-                        console.log(paragraphNode)
+
+                        if (i >= boldMatches.length - 1) {
+                            const textNode: TNode = { ...text, content: content };
+                            paragraphNode.children?.push(textNode)
+                        }
                     }
+                    rootNode.children?.push(paragraphNode)
+
                 }
-
-                // for (let i = 0; i < contentParts.length; i++) {
-                //     const contentPart = contentParts[i]
-                //     // 跳过第一个
-                //     if (contentPart) {
-                //         const [boldText, text] = content.split('</b>');
-                //         console.log(boldText, text)
-                //     }
-                //     if (i !== 0) {
-                //         // 添加文本节点
-                //         const textNode: TNode = { type: 'text', attrs: {}, content: contentParts[i].split('</b>')[0] };
-                //         boldNode.children?.push(textNode);
-                //     }
-                //     console.log(boldNode.children)
-                //     // 添加粗体节点
-                //     const bTagContent = contentParts[i].substring(contentParts[i].indexOf('</b>') + 4);
-                //     if (bTagContent) {
-                //         const newBoldNode: TNode = { ...boldNode };
-                //         paragraphNode.children?.push(newBoldNode);
-                //         // console.log(paragraphNode.children)
-                //     }
-                // }
-                // if (boldNode.children && boldNode.children.length > 0) {
-                //     paragraphNode.children?.push(boldNode);
-                // }
             } else {
-                // 加粗
                 // 添加文本节点
-                // const paragraphNode: TNode = { type: 'p', attrs: {}, content: content, children: [] };
-                paragraphNode.content = content
-                const textNode: TNode = { type: 'text', attrs: {}, content: content };
-                console.log(content)
-                // paragraphNode.children?.push(textNode);
-                // rootNode.children?.push(paragraphNode);
+                // paragraphNode.content = content
+                const textNode: TNode = { ...text, content: content };
+                paragraphNode.children?.push(textNode);
+                rootNode.children?.push(paragraphNode);
             }
-            rootNode.children?.push(paragraphNode);
             console.log(rootNode)
-
-
-        }
-        // 可以添加其他处理不同类型节点的逻辑
+        // 添加其他处理不同类型节点的逻辑
     }
     return rootNode;
 }
@@ -201,13 +142,9 @@ function BoldText() {
     const range: Range = selection.getRangeAt(0);
     const selectedText: string = range.toString();
     // // 选中文本的节点标签
-    // const startNode: Node | null = range.startContainer.parentNode
-    // const endNode: Node | null = range.endContainer.parentNode
-    // if (startNode?.nodeName !== 'B' ) {
     const boldText: string = `<b>${selectedText}</b>`;
     range.deleteContents();
     range.insertNode(document.createRange().createContextualFragment(boldText));
-    // }
     selection.removeAllRanges();
     handleInput
 }
@@ -224,7 +161,7 @@ function save() {
         <div class="content" ref="editor" contenteditable="true" @input="handleInput">
             <p><br /></p>
         </div>
-        <div class="testboc" v-html="renderNodeTree(state)"></div>
+        <!-- <div class="testbox" v-html="renderNodeTree(state)"></div> -->
     </div>
 </template>
 <style scoped lang="less">
@@ -242,7 +179,6 @@ function save() {
         height: 100%;
         padding: 40px;
     }
-
     .content:empty::before {
         content: attr(placeholder);
     }
