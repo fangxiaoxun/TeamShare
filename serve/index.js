38c0f5b3bf6e6f287821c98c16d9311425a12b47
FYQ <Fanyinqian@guigu.com>
2023-07-17 18:12:26 +0800

    to:删除没用的代码

--- serve/index.js
+++ serve/index.js
@@ -40,16 +40,6 @@ app.use(
 app.use('/user', require('./router/user.router.js'))
 app.use('/folder',require('./router/folder.router.js'))
 app.use('/file',require('./router/file.router.js'))
-// app.get('/getUser',(req,res)=>{
-//     let sql = 'select * from user'
-//     conMysql(sql).then(result => {
-//         res.send({
-// 			code:200,
-// 			data:result
-// 		})
-//     })
-
-// })
 
 
 // token过期或不合法
